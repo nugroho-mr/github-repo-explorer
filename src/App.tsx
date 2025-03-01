9e6cc79705b309a87b942245590aae7ff5272c74
Panca Satrio Nugroho <nugroho.panca@gmail.com>
2025-03-01 11:45:20 +0700

    setup basic vite

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-5xl font-bold">Hello Vite + React + Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
